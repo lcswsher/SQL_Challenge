a7bc93d0db9edc40b944c15922d01beaa88912f9
lcswsher <lcswisher@yahoo.com>
2021-06-18 22:49:26 -0500

    Adding preliminary Data Analysis Queries

---
+++ EmployeeSQL/SQL_Challenge_Data_Analysis_Queries.sql
@@ -0,0 +1,26 @@
+-- Data Analysis Queries 
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees as e
+join salaries as s on e.emp_no = s.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1986-01-01' and '1986-12-31'
+order by hire_date;
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
